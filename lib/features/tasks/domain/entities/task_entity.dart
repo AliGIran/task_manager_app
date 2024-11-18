f2a8e711f99bc4dd296e5f1393c882ac4e5a36c6
AliGIran <a.gorji013@gmail.com>
2024-11-18 06:12:05 +0330

    mockup of page is created by temp list

--- lib/features/tasks/domain/entities/task_entity.dart
+++ lib/features/tasks/domain/entities/task_entity.dart
@@ -3,14 +3,14 @@ import 'dart:ui';
 class TaskEntity {
   final String title;
   final String? desc;
-  final Color color;
+  final Color? color;
   final List? subTasks;
   bool isDone;
 
   TaskEntity(
       {required this.title,
       this.desc,
-      required this.color,
+      this.color,
       this.subTasks,
       this.isDone = false});
 }
--- lib/features/tasks/presentation/pages/tasks_home_page.dart
+++ lib/features/tasks/presentation/pages/tasks_home_page.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/features/tasks/domain/entities/task_entity.dart';
 
+List<TaskEntity> tempList = [
+  TaskEntity(title: "title 1", color: Colors.red),
+  TaskEntity(title: "title 2", color: Colors.blue),
+  TaskEntity(title: "title 3", color: Colors.yellow),
+  TaskEntity(title: "title 4", color: Colors.green),
+  TaskEntity(title: "title 5", ),
+  TaskEntity(title: "title 6", )
+];
 
 class TasksHomePage extends StatelessWidget {
   const TasksHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("کارهای من"),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          // todo: change list from temp to db
+
+          itemCount: tempList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: tempList[index].color,
+              child: CheckboxListTile(
+                secondary: IconButton(
+                    onPressed: () {
+                      //   todo: add remove function
+                    },
+                    icon: const Icon(Icons.delete_rounded)),
+                title: Text(tempList[index].title),
+                value: tempList[index].isDone,
+                onChanged: (bool? value) {
+                  //   todo: add change value function
+                  tempList[index].isDone = value!;
+                },
+              ),
+            );
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add_rounded),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
